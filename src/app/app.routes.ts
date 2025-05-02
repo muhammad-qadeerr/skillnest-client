c6f67c5445419181812cf0ccab7e2a8346b4a382
Muhammad Qadeer <muhammad.qadeer@softmatic.io>
2025-05-02 23:43:08 +0500

    skillnest client initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { DataBindingsComponent } from './components/data-binding/data-binding.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    //{path: 'data-bindings', component: DataBindingsComponent},
+
+    // Implementing lazy loading for data-bindings module
+    {path: 'data-bindings', component: DataBindingsComponent},
+
+    {path: 'about', component: AboutComponent},
+    {path: 'contact-us', component: ContactUsComponent},
+
+    // if the route is not found, redirect to home
+    {path: '**', redirectTo: 'home'}
+];
---
+++ src/app/pages/about/about.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({  // from angular 18 onwards, all the angular components are standalone by default
+  selector: 'app-about',
+  imports: [],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css'
+})
+export class AboutComponent {
+
+}
